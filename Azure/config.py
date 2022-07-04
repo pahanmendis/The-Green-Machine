d8ef9cfd18bb15880d6c014d1113b73e1928b8c6
pahanmendis <53089732+pahanmendis@users.noreply.github.com>
2022-07-04 20:15:24 +0530

    Update config file for security
    Updated the configuration file of the Cosmos DB for security reasons

--- Azure/config.py
+++ Azure/config.py
@@ -1,8 +1,8 @@
 import os
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://ap116-sql-data.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'PUMtMoawiui1XdquF7X1eb0LU75bPa7Qb1iTWFjbO7qPlhPTKx9S5f3j7eTpKNrP4c0FLmFnqTNiZlDcZGVG6w=='),
+    'host': os.environ.get('ACCOUNT_HOST', 'host'),
+    'master_key': os.environ.get('ACCOUNT_KEY', 'key'),
     'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
     'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
 }
